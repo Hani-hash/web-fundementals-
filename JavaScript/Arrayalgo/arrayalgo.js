78a7b62afcf0dba6d1ce3356e9e085ca55961001
Hani-hash <hani.ikhmais@gmail.com>
2021-05-03 14:06:03 +0300

    HANI

---
+++ JavaScript/Arrayalgo/arrayalgo.js
@@ -0,0 +1,7 @@
+var testArr = [6,3,5,1,2,4]
+var sum = 0;
+for (var i=0; i<testArr.length; i++){
+    sum=sum+testArr[i];
+    console.log("num: " + testArr[i] +"sum:" + sum)
+  } 
+ 
---
+++ JavaScript/Arrayalgo/secondQ.js
@@ -0,0 +1,6 @@
+var testArr = [6,3,5,1,2,4]
+
+for (var i=0; i<testArr.length; i++){
+    testArr[i]= i*testArr[i];
+    console.log(testArr[i])
+}
